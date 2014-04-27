60a62931d6fefd0d38062e48ba9467721bb86985
bavent <bavent@vt.edu>
2014-04-27 01:23:49 -0400

    Added some basic hypergraph utility methods.

--- hypergraph/hypergraph.py
+++ hypergraph/hypergraph.py
@@ -47,6 +47,25 @@ class HyperGraph:
                 return n
         return None
 
+	def get_nodes_by_name(self, node_names):
+		'''
+		Takes a set of names and returns the corresponding set of node objects
+		'''
+		node_set = set()
+		for node in self.nodes:
+			if node.name in node_names:
+				node_set.add(node)
+		return node_set
+
+	def get_node_names(self):
+		'''
+		Helper function to return the names of all the nodes in the graph
+		'''
+		name_set = set()
+		for node in self.nodes:
+			set.add(node.name)
+		return name_set
+
     def remove_hypernode(self, n):
         '''
         Removes a hypernode from the graph.
@@ -197,13 +216,97 @@ class DirectedHyperGraph(HyperGraph):
             tail = words[0].split(delim)
             head = words[1].split(delim)
             try:
-                weight = float(words[2].split(delim)[0])
-            except:
-                weight = 0
-
-            # Create hypergraph from current line
+               weight = float(words[2].split(delim)[0])
+            except: weight = 0
+           
+            # Create hypergraph from current line            
             self.add_hyperedge(head, tail, weight)
 
+	def symmetric_image(self):
+		'''
+		Returns a new hypergraph that is the symmetric image of this hypergraph
+		'''
+		nodes = deepcopy(self.nodes)
+		hyperedges = deepcopy(self.hyperedges)
+		for e in hyperedges:
+			# TODO: put @property setters in hyperedge class?
+			e._tail, e._head = e.head, e.tail
+		return DirectedHyperGraph(nodes, hyperedges) # what if it's now a DirectedHyperArcGraph?
+
+	def b_visit(self, s):
+		'''
+		Returns the set of all nodes that are B-Connected to s
+		'''
+		# TODO: implement B-Visit method
+		pass
+
+	def b_connection(self, s):
+		'''
+		Alias for b_visit
+		'''
+		return self.b_visit(s)
+
+	def f_visit(self, s):
+		'''
+		Returns the set of all nodes that are F-Connected to node s
+		'''
+		symmetric_image = self.symmetric_image()
+		s = symmetric_image.get_node_by_name(s.name)
+
+		symmetric_b_visit = symmetric_image.b_visit(s)
+
+		f_connected_node_names = self.get_node_names(symmetric_b_visit)
+		return self.get_nodes_by_name(f_connected_node_names)
+
+	def f_connection(self, s):
+		'''
+		Alias for f_visit
+		'''
+		return self.f_visit(s)
+
+	def is_b_connected(self, *arg):
+		'''
+		Determines if a node is B-Connected to other nodes
+		usage:
+			1st argument is node 's'
+			if 1 argument is provided:
+				determines if all nodes are B-Connected to 's'
+			if 2 arguments are provided:
+				determines if the node 't' specified by the 2nd argument is B-Connected to 's'
+		'''
+		if len(arg) == 1:
+			s = arg[0]
+			b_connected_nodes = self.b_visit(s)
+			return b_connected_nodes == self.nodes
+		elif len(arg) == 2:
+			s = arg[0]
+			t = arg[1]
+			b_connected_nodes = self.b_visit(s)
+			return t in b_connected_nodes
+		else:
+			raise ValueError('Invalid number of arguments {}'.format(len(arg)))
+
+	def is_f_connected(self, *arg)
+		'''
+		Determines if a node is F-Connected to other nodes
+		usage:
+			1st argument is node 's'
+			if 1 argument is provided:
+				determines if all nodes are F-Connected to 's'
+			if 2 arguments are provided:
+				determines if the node 't' specified by the 2nd argument is F-Connected to 's'
+		'''
+		if len(arg) == 1:
+			symmetric_image = self.symmetric_image()
+			s = symmetric_image.get_node_by_name(arg[0].name)
+			return symmetric_image.is_b_connected(s)
+		elif len(arg) == 2:
+			symmetric_image = self.symmetric_image()
+			s = symmetric_image.get_node_by_name(arg[0].name)
+			t = symmetric_image.get_node_by_name(arg[1].name)
+			return symmetric_image.is_b_connected(t, s)
+		else:
+			raise ValueError('Invalid number of arguments {}'.format(len(arg)))
 
 class DirectedHyperArcGraph(HyperGraph):
     pass

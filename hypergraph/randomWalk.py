9c2f43563119d4c671e4102b8dc2a715907688d2
Antuan <antuan@antuan-VirtualBox.(none)>
2014-05-12 14:06:31 -0400

    Skeleton for random walk functions and new branch

---
+++ hypergraph/randomWalk.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+from .hypergraph import HyperGraph
+from copy import deepcopy
+
+
+'''----------------------- Random Walk Functions -----------------------------'''
+def build_incidence_matrix():
+	''' stuff '''
+	return 1;
+
+def build_transition_matrix():
+	''' stuff '''
+	return 1;
+

347bf889fe61ffc8d1e63d97318d2776de37b7b5
bavent <bavent@vt.edu>
2014-06-11 18:51:34 -0400

    Initial commit of [unfinished] minimal base-functionality (for collaborative referencing purposes)

--- hypergraph/directed_hypergraph.py
+++ hypergraph/directed_hypergraph.py
@@ -1,9 +1,29 @@
+"""
+.. module:: directed_hypergraph
+   :synopsis: Defines DirectedHypergraph class for the basic properties
+            of a directed hypergraph, along with the relevant structures
+            regarding nodes, hyperedges, adjacency, etc.
+
+"""
+
 from __future__ import absolute_import
 
 
 class DirectedHypergraph(object):
+    """
+    DirectedHypergraph class provides a basic directed hypergraph object
+    and typical functions for the basic properties of the structure.
+
+    TODO: Put a lot of information here about the structures (mostly the
+        ones instantiated by the constuctor).
+
+    """
 
     def __init__(self):  # , hyperedges=None, node_ordering=None):
+        """
+        Constructor for the DirectedHypergraph class.
+
+        """
         self._node_attributes = {}
         self._hyperedge_attributes = {}
         self._forward_star = {}
@@ -12,59 +32,10 @@ class DirectedHypergraph(object):
         self._predecessors = {}
         self._assigned_hyperedges = 0
 
-    @property
-    def node_attributes(self):
-        return self._node_attributes
+    def _combine_attribute_arguments(self, attr_dict, attr):
+        """Combines attr_dict and attr dictionaries, by updating attr_dict
+            with attr.
 
-    @node_attributes.setter
-    def node_attributes(self, value):
-        self._node_attributes = value
-
-    @property
-    def hyperedge_attributes(self):
-        return self._hyperedge_attributes
-
-    @hyperedge_attributes.setter
-    def hyperedge_attributes(self, value):
-        self._hyperedge_attributes = value
-
-    @property
-    def forward_star(self):
-        return self._forward_star
-
-    @forward_star.setter
-    def forward_star(self, value):
-        self._forward_star = value
-
-    @property
-    def backward_star(self):
-        return self._backward_star
-
-    @backward_star.setter
-    def backward_star(self, value):
-        self._backward_star = value
-
-    @property
-    def successors(self):
-        return self._successors
-
-    @successors.setter
-    def successors(self, value):
-        self._successors = value
-
-    @property
-    def predecessors(self):
-        return self._predecessors
-
-    @predecessors.setter
-    def predecessors(self, value):
-        self._predecessors = value
-
-    def add_node(self, node, attr_dict=None, **attr):
-        """Adds a node to the graph, along with any related attributes
-           of the node.
-
-        :param node: reference of the node.
         :param attr_dict: dictionary of attributes of the node.
         :param **attr: keyword arguments of attributes of the node; takes
                     precedence over attr_dict.
@@ -83,16 +54,30 @@ class DirectedHypergraph(object):
             except AttributeError:
                 raise AttributeError("attr_dict argument \
                                      must be a dictionary.")
+        return attr_dict
+
+    def add_node(self, node, attr_dict=None, **attr):
+        """Adds a node to the graph, along with any related attributes
+           of the node.
+
+        :param node: reference of the node.
+        :param attr_dict: dictionary of attributes of the node.
+        :param **attr: keyword arguments of attributes of the node; takes
+                    precedence over attr_dict.
+        :raises: AttributeError
+
+        """
+        attr_dict = self._combine_attribute_arguments(attr_dict, attr)
 
         # If the node hasn't previously been added, add it along
         # with its attributes
-        if node not in self.node_attributes:
-            self.node_attributes[node] = attr_dict
-            self.forward_star[node] = set()
-            self.backward_star[node] = set()
+        if node not in self._node_attributes:
+            self._node_attributes[node] = attr_dict
+            self._forward_star[node] = set()
+            self._backward_star[node] = set()
         # Otherwise, just update the node's attributes
         else:
-            self.node_attributes[node].update(attr_dict)
+            self._node_attributes[node].update(attr_dict)
 
     def add_nodes(self, nodes, attr_dict=None, **attr):
         """Adds multiple nodes to the graph, along with any related attributes
@@ -107,18 +92,7 @@ class DirectedHypergraph(object):
         :raises: AttributeError
 
         """
-        # If no attribute dict was passed, treat the keyword
-        # arguments as the dict
-        if attr_dict is None:
-            attr_dict = attr
-        # Otherwise, combine the passed attribute dict with the
-        # keyword arguments
-        else:
-            try:
-                attr_dict.update(attr)
-            except AttributeError:
-                raise AttributeError("attr_dict argument \
-                                     must be a dictionary.")
+        attr_dict = self._combine_attribute_arguments(attr_dict, attr)
 
         for node in nodes:
             # Note: This won't behave properly if the node is actually a tuple
@@ -146,29 +120,18 @@ class DirectedHypergraph(object):
         :raises: AttributeError
 
         """
-        # If no attribute dict was passed, treat the keyword
-        # arguments as the dict
-        if attr_dict is None:
-            attr_dict = attr
-        # Otherwise, combine the passed attribute dict with the
-        # keyword arguments
-        else:
-            try:
-                attr_dict.update(attr)
-            except AttributeError:
-                raise AttributeError("attr_dict argument \
-                                     must be a dictionary.")
+        attr_dict = self._combine_attribute_arguments(attr_dict, attr)
 
         # Use frozensets for tail and head sets to allow for hashable keys
         frozen_tail = frozenset(tail)
         frozen_head = frozenset(head)
 
-        if frozen_tail not in self.successors:
-            self.successors[frozen_tail] = {}
-        if frozen_head not in self.predecessors:
-            self.predecessors[frozen_head] = {}
+        if frozen_tail not in self._successors:
+            self._successors[frozen_tail] = {}
+        if frozen_head not in self._predecessors:
+            self._predecessors[frozen_head] = {}
 
-        is_new_hyperedge = frozen_head not in self.successors[frozen_tail]
+        is_new_hyperedge = frozen_head not in self._successors[frozen_tail]
         if is_new_hyperedge:
             # Add tail and head nodes to graph (if not already present)
             self.add_nodes(frozen_head)
@@ -178,21 +141,21 @@ class DirectedHypergraph(object):
             self._assigned_hyperedges += 1
             hyperedge_name = "e" + str(self._assigned_hyperedges)
 
-            # Add edge to the backward-star and to the forward-star for each
+            # Add edge to the forward-star and to the backward-star for each
             # node in the tail and head sets, respectively
             for node in frozen_tail:
-                self.backward_star[node].add(hyperedge_name)
+                self._forward_star[node].add(hyperedge_name)
             for node in frozen_head:
-                self.forward_star[node].add(hyperedge_name)
+                self._backward_star[node].add(hyperedge_name)
 
             # Add the edge as the successors and predecessors of the tail set
             # and head set, respectively
-            self.successors[frozen_tail][frozen_head] = hyperedge_name
-            self.predecessors[frozen_head][frozen_tail] = hyperedge_name
+            self._successors[frozen_tail][frozen_head] = hyperedge_name
+            self._predecessors[frozen_head][frozen_tail] = hyperedge_name
 
-            self.hyperedge_attributes[hyperedge_name] = \
+            self._hyperedge_attributes[hyperedge_name] = \
                 {'tail': tail, 'head': head}
         else:
-            hyperedge_name = self.successors[frozen_tail][frozen_head]
+            hyperedge_name = self._successors[frozen_tail][frozen_head]
 
-        self.hyperedge_attributes[hyperedge_name].update(attr_dict)
+        self._hyperedge_attributes[hyperedge_name].update(attr_dict)
--- tests/test_directed_hypergraph.py
+++ tests/test_directed_hypergraph.py
@@ -18,22 +18,22 @@ def test_add_node():
     H.add_node(node_c, attrib_c)
     H.add_node(node_d, attrib_d, sink=False)
 
-    assert node_a in H.node_attributes
-    assert H.node_attributes[node_a] == {}
-    
-    assert node_b in H.node_attributes
-    assert H.node_attributes[node_b]['source'] == True
-    
-    assert node_c in H.node_attributes
-    assert H.node_attributes[node_c]['alt_name'] == 1337
-    
-    assert node_d in H.node_attributes
-    assert H.node_attributes[node_d]['label'] == 'black'
-    assert H.node_attributes[node_d]['sink'] == False
+    assert node_a in H._node_attributes
+    assert H._node_attributes[node_a] == {}
+
+    assert node_b in H._node_attributes
+    assert H._node_attributes[node_b]['source'] is True
+
+    assert node_c in H._node_attributes
+    assert H._node_attributes[node_c]['alt_name'] == 1337
+
+    assert node_d in H._node_attributes
+    assert H._node_attributes[node_d]['label'] == 'black'
+    assert H._node_attributes[node_d]['sink'] is False
 
     # Test adding a node that has already been added
     H.add_nodes(node_a, common=False)
-    assert H.node_attributes[node_a]['common'] == False
+    assert H._node_attributes[node_a]['common'] is False
 
 
 def test_add_nodes():
@@ -44,25 +44,26 @@ def test_add_nodes():
     node_d = 'D'
     attrib_d = {'label': 'black', 'sink': True}
     common_attrib = {'common': True, 'source': False}
-    
-    node_list = [node_a, (node_b, {'source': False}), (node_c, attrib_c), (node_d, attrib_d)]
+
+    node_list = [node_a, (node_b, {'source': False}),
+                 (node_c, attrib_c), (node_d, attrib_d)]
 
     # Test adding unadded nodes with various attribute settings
     H = DirectedHypergraph()
     H.add_nodes(node_list, common_attrib)
 
-    assert node_a in H.node_attributes
-    assert H.node_attributes[node_a] == common_attrib
-    
-    assert node_b in H.node_attributes
-    assert H.node_attributes[node_b]['source'] == False
-    
-    assert node_c in H.node_attributes
-    assert H.node_attributes[node_c]['alt_name'] == 1337
-    
-    assert node_d in H.node_attributes
-    assert H.node_attributes[node_d]['label'] == 'black'
-    assert H.node_attributes[node_d]['sink'] == True
+    assert node_a in H._node_attributes
+    assert H._node_attributes[node_a] == common_attrib
+
+    assert node_b in H._node_attributes
+    assert H._node_attributes[node_b]['source'] is False
+
+    assert node_c in H._node_attributes
+    assert H._node_attributes[node_c]['alt_name'] == 1337
+
+    assert node_d in H._node_attributes
+    assert H._node_attributes[node_d]['label'] == 'black'
+    assert H._node_attributes[node_d]['sink'] is True
 
 
 def test_add_hyperedge():
@@ -71,8 +72,11 @@ def test_add_hyperedge():
     node_c = 'C'
     node_d = 'D'
 
-    tail = set([node_a, node_b]); frozen_tail = frozenset(tail)
-    head = set([node_c, node_d]); frozen_head = frozenset(head)
+    tail = set([node_a, node_b])
+    head = set([node_c, node_d])
+    frozen_tail = frozenset(tail)
+    frozen_head = frozenset(head)
+
     attrib = {'weight': 6, 'color': 'black'}
 
     H = DirectedHypergraph()
@@ -80,14 +84,20 @@ def test_add_hyperedge():
     H.add_hyperedge(tail, head, attrib, weight=5)
 
     # Test that all hyperedge attributes are correct
-    assert H.hyperedge_attributes['e1']['tail'] == tail
-    assert H.hyperedge_attributes['e1']['head'] == head
-    assert H.hyperedge_attributes['e1']['weight'] == 5
+    assert H._hyperedge_attributes['e1']['tail'] == tail
+    assert H._hyperedge_attributes['e1']['head'] == head
+    assert H._hyperedge_attributes['e1']['weight'] == 5
 
     # Test that successor list contains the correct info
-    assert frozen_head in H.successors[frozen_tail]
-    assert 'e1' in H.successors[frozen_tail][frozen_head]
+    assert frozen_head in H._successors[frozen_tail]
+    assert 'e1' in H._successors[frozen_tail][frozen_head]
 
     # Test that the precessor list contains the correct info
-    assert frozen_tail in H.predecessors[frozen_head]
-    assert 'e1' in H.predecessors[frozen_head][frozen_tail]
+    assert frozen_tail in H._predecessors[frozen_head]
+    assert 'e1' in H._predecessors[frozen_head][frozen_tail]
+
+    # Test that forward-stars and backward-stars contain the correct info
+    for node in frozen_tail:
+        assert 'e1' in H._forward_star[node]
+    for node in frozen_head:
+        assert 'e1' in H._backward_star[node]

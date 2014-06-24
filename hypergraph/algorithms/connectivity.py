ec3b12c6b2c96c03b9b407d5c36ccb4388b892a6
bavent <bavent@vt.edu>
2014-06-24 14:44:02 -0400

    Added F-Visit functionality; generalized B- and F-Visit algorithms to a single X-Visit algorithm.

--- hypergraph/algorithms/connectivity.py
+++ hypergraph/algorithms/connectivity.py
@@ -37,6 +37,7 @@ def visit(hypergraph, source_node):
                     node wasn't visited or if that node is the source node
               dict -- mapping from hyperedge IDs to the nodes that preceeded
                     them in this traversal
+    :raises: TypeError -- Algorithm only applicable to directed hypergraphs
 
     """
     if not isinstance(hypergraph, DirectedHypergraph):
@@ -93,25 +94,42 @@ def is_connected(hypergraph, source_node, target_node):
     return target_node in visited_nodes
 
 
-def b_visit(hypergraph, source_node):
-    """Executes the 'B-Visit' algorithm described in the paper:
-    Giorgio Gallo, Giustino Longo, Stefano Pallottino, Sang Nguyen,
-    Directed hypergraphs and applications, Discrete Applied Mathematics,
-    Volume 42, Issues 2-3, 27 April 1993, Pages 177-201, ISSN 0166-218X,
-    http://dx.doi.org/10.1016/0166-218X(93)90045-P.
-    (http://www.sciencedirect.com/science/article/pii/0166218X9390045P)
+def _x_visit(hypergraph, source_node, b_visit):
+    """General form of the B-Visit algorithm, extended to also perform
+    an implicit F-Visit if the b_visit flag is not set (providing better
+    time/memory performance than explcitily taking the hypergraph's
+    symmetric image and then performing the B-Visit on that).
 
     :param hypergraph: the hypergraph to perform the 'B-Visit' algorithm on
     :param source_node: the initial node to begin traversal from
+    :param b_visit: boolean flag representing whether a B-Visit should
+                    be performed
     :returns: set -- nodes that were visited in this traversal
               dict -- mapping from nodes to the ID hyperedges that preceeded
                     them in this traversal
               dict -- mapping from hyperedge IDs to the nodes that preceeded
                     them in this traversal
+              dict -- mapping from nodes to an integer representing the
+                    cardinality of the path from the source node to each
+                    of the nodes
+    :raises: TypeError -- Algorithm only applicable to directed hypergraphs
 
     """
     if not isinstance(hypergraph, DirectedHypergraph):
         raise TypeError("Algorithm only applicable to directed hypergraphs")
+
+    # If the b_visit flag is set, perform a traditional B-Visit
+    if b_visit:
+        forward_star = hypergraph.get_forward_star
+        hyperedge_tail = hypergraph.get_hyperedge_tail
+        hyperedge_head = hypergraph.get_hyperedge_head
+    # If the b_visit flag is not set, implicitly perform an F-Visit by
+    # implicitly taking the symmetric image (what the 'else' statement
+    # is for) and then performing a traditional B-Visit
+    else:
+        forward_star = hypergraph.get_backward_star
+        hyperedge_tail = hypergraph.get_hyperedge_head
+        hyperedge_head = hypergraph.get_hyperedge_tail
 
     node_set = hypergraph.get_node_set()
     # Pv keeps track of the ID of the hyperedge that directely
@@ -134,7 +152,7 @@ def b_visit(hypergraph, source_node):
     k = {hyperedge_id: 0 for hyperedge_id in hyperedge_id_set}
 
     # Explicitly tracks the set of B-visited nodes
-    b_visited_nodes = set(source_node)
+    x_visited_nodes = set(source_node)
 
     Q = Queue()
     Q.put(source_node)
@@ -142,27 +160,50 @@ def b_visit(hypergraph, source_node):
     while not Q.empty():
         current_node = Q.get()
         # At current_node, we can traverse each hyperedge in its forward star
-        for hyperedge_id in hypergraph.get_forward_star(current_node):
+        for hyperedge_id in forward_star(current_node):
             # Since we're arrived at a new node, we increment
             # k[hyperedge_id] to indicate that we've reached 1 new
             # node in this hyperedge's tail
-            k[hyperedge_id] = k[hyperedge_id] + 1
+            k[hyperedge_id] += 1
             # Traverse this hyperedge only when we have reached all the nodes
             # in its tail (i.e., when k[hyperedge_id] == |T(hyperedge_id)|)
             if k[hyperedge_id] == \
-               len(hypergraph.get_hyperedge_tail(hyperedge_id)):
+               len(hyperedge_tail(hyperedge_id)):
                 Pe[hyperedge_id] = current_node
                 # Traversing the hyperedge yields the set of head nodes of
                 # the hyperedge; B-visit each head node
-                for head_node in hypergraph.get_hyperedge_head(hyperedge_id):
-                    if head_node in b_visited_nodes:
+                for head_node in hyperedge_head(hyperedge_id):
+                    if head_node in x_visited_nodes:
                         continue
                     Pv[head_node] = hyperedge_id
                     Q.put(head_node)
                     v[head_node] = v[Pe[hyperedge_id]] + 1
-                    b_visited_nodes.add(head_node)
+                    x_visited_nodes.add(head_node)
 
-    return b_visited_nodes, Pv, Pe, v
+    return x_visited_nodes, Pv, Pe, v
+
+
+def b_visit(hypergraph, source_node):
+    """Executes the 'B-Visit' algorithm described in the paper:
+    Giorgio Gallo, Giustino Longo, Stefano Pallottino, Sang Nguyen,
+    Directed hypergraphs and applications, Discrete Applied Mathematics,
+    Volume 42, Issues 2-3, 27 April 1993, Pages 177-201, ISSN 0166-218X,
+    http://dx.doi.org/10.1016/0166-218X(93)90045-P.
+    (http://www.sciencedirect.com/science/article/pii/0166218X9390045P)
+
+    :param hypergraph: the hypergraph to perform the 'B-Visit' algorithm on
+    :param source_node: the initial node to begin traversal from
+    :returns: set -- nodes that were visited in this traversal
+              dict -- mapping from nodes to the ID hyperedges that preceeded
+                    them in this traversal
+              dict -- mapping from hyperedge IDs to the nodes that preceeded
+                    them in this traversal
+              dict -- mapping from nodes to an integer representing the
+                    cardinality of the path from the source node to each
+                    of the nodes
+
+    """
+    return _x_visit(hypergraph, source_node, True)
 
 
 def is_b_connected(hypergraph, source_node, target_node):
@@ -183,3 +224,41 @@ def is_b_connected(hypergraph, source_node, target_node):
     """
     b_visited_nodes, Pv, Pe, v = b_visit(hypergraph, source_node)
     return target_node in b_visited_nodes
+
+
+def f_visit(hypergraph, source_node):
+    """Executes the 'F-Visit' algorithm alluded to in the paper:
+    Giorgio Gallo, Giustino Longo, Stefano Pallottino, Sang Nguyen,
+    Directed hypergraphs and applications, Discrete Applied Mathematics,
+    Volume 42, Issues 2-3, 27 April 1993, Pages 177-201, ISSN 0166-218X,
+    http://dx.doi.org/10.1016/0166-218X(93)90045-P.
+    (http://www.sciencedirect.com/science/article/pii/0166218X9390045P)
+
+    :param hypergraph: the hypergraph to perform the 'F-Visit' algorithm on
+    :param source_node: the initial node to begin traversal from
+    :returns: set -- nodes that were visited in this traversal
+              dict -- mapping from nodes to the ID hyperedges that preceeded
+                    them in this traversal
+              dict -- mapping from hyperedge IDs to the nodes that preceeded
+                    them in this traversal
+              dict -- mapping from nodes to an integer representing the
+                    cardinality of the path from the source node to each
+                    of the nodes
+
+    """
+    return _x_visit(hypergraph, source_node, False)
+
+
+def is_f_connected(hypergraph, source_node, target_node):
+    """Checks if a target node is F-connected to a source node.
+    A node t is F-connected to a node s iff s if B-connected to t.
+    Refer to 'B-connected's documentation for more details.
+
+    :param hypergraph: the hypergraph to check F-connectedness on
+    :param source_node: the node to check F-connectedness to
+    :param target_node: the node to check F-connectedness of
+    :returns: bool -- whether target_node can be visited from source_node
+
+    """
+    f_visited_nodes, Pv, Pe, v = f_visit(hypergraph, source_node)
+    return target_node in f_visited_nodes
--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -51,6 +51,7 @@ def test_b_visit():
     H = DirectedHypergraph()
     H.read("tests/data/basic_directed_hypergraph.txt")
 
+    # Let 's' be the source node:
     b_visited_nodes, Pv, Pe, v = connectivity.b_visit(H, 's')
 
     assert b_visited_nodes == set(['s', 'x', 'y', 'z', 't', 'u'])
@@ -71,6 +72,19 @@ def test_b_visit():
     assert Pe['e7'] is None
     assert Pv['b'] is None
 
+    # Let 't' be the source node:
+    b_visited_nodes, Pv, Pe, v = connectivity.b_visit(H, 't')
+
+    assert b_visited_nodes == set(['t'])
+
+    assert Pv['t'] is None
+    assert (Pv['s'], Pv['x'], Pv['y'], Pv['z'], Pv['u'], Pv['a'], Pv['b']) == \
+        (None, None, None, None, None, None, None)
+    assert (Pe["e1"], Pe["e2"], Pe["e3"], Pe["e4"],
+            Pe["e5"], Pe["e6"], Pe["e7"]) == \
+        (None, None, None, None, None, None, None)
+
+    # Try an invalid B-Visit
     try:
         connectivity.b_visit('s', 't')
         assert False
@@ -92,3 +106,56 @@ def test_is_b_connected():
     assert connectivity.is_b_connected(H, 's', 'u')
     assert not connectivity.is_b_connected(H, 's', 'a')
     assert not connectivity.is_b_connected(H, 's', 'b')
+
+
+def test_f_visit():
+    H = DirectedHypergraph()
+    H.read("tests/data/basic_directed_hypergraph.txt")
+
+    # Let 's' be the source node:
+    f_visited_nodes, Pv, Pe, v = connectivity.f_visit(H, 's')
+
+    assert f_visited_nodes == set(['s', 'x'])
+
+    assert Pv['s'] is None
+    assert v['s'] == 0
+    assert Pe['e1'] == 'x'
+    assert (Pe['e2'], Pe['e3'], Pe['e4'], Pe['e5'], Pe['e7']) == \
+        (None, None, None, None, None)
+    assert (Pv['y'], Pv['z'], Pv['u'], Pv['t'], Pv['a'], Pv['b']) == \
+        (None, None, None, None, None, None)
+
+    # Let 't' be the source node:
+    f_visited_nodes, Pv, Pe, v = connectivity.f_visit(H, 't')
+
+    assert f_visited_nodes == set(['t'])
+    
+    assert Pv['t'] is None
+    assert (Pv['s'], Pv['x'], Pv['y'], Pv['z'], Pv['u'], Pv['a'], Pv['b']) == \
+        (None, None, None, None, None, None, None)
+    assert (Pe["e1"], Pe["e2"], Pe["e3"], Pe["e4"],
+            Pe["e5"], Pe["e6"], Pe["e7"]) == \
+        (None, None, None, None, None, None, None)
+
+    # Try invalid F-visit
+    try:
+        connectivity.f_visit('s', 't')
+        assert False
+    except TypeError:
+        pass
+    except BaseException as e:
+        assert False, e
+
+
+def test_is_f_connected():
+    H = DirectedHypergraph()
+    H.read("tests/data/basic_directed_hypergraph.txt")
+
+    assert connectivity.is_f_connected(H, 's', 's')
+    assert connectivity.is_f_connected(H, 's', 'x')
+    assert not connectivity.is_f_connected(H, 's', 'y')
+    assert not connectivity.is_f_connected(H, 's', 'z')
+    assert not connectivity.is_f_connected(H, 's', 't')
+    assert not connectivity.is_f_connected(H, 's', 'u')
+    assert not connectivity.is_f_connected(H, 's', 'a')
+    assert not connectivity.is_f_connected(H, 's', 'b')

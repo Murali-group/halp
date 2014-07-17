6bd6c8818cac697300d977767fbcc69027c9858a
bavent <bavent@vt.edu>
2014-07-17 18:55:59 -0400

    Brought graphspace utilities file and tests up to pep8.

--- tests/utilities/test_graphspace.py
+++ tests/utilities/test_graphspace.py
@@ -1,9 +1,12 @@
 from hypergraph.directed_hypergraph import DirectedHypergraph
 from hypergraph.utilities import graphspace
 
+
 def test_graphspace():
     H = DirectedHypergraph()
     H.read("tests/data/basic_directed_hypergraph.txt")
 
-    graphspace.post_to_graphspace(H,'basic_directed_hypergraph',\
-                                      'annaritz@vt.edu','platypus')
+    graphspace.post_to_graphspace(H,
+                                  'basic_directed_hypergraph',
+                                  'annaritz@vt.edu',
+                                  'platypus')

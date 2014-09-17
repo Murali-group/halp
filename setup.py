1c34417ae2270ed1f1b8026c0222378c29cd1edb
Brendan Avent <bavent@vt.edu>
2014-09-17 02:41:00 -0400

    Updated distutils info.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
+from distutils.core import setup
 
 
 class PyTest(TestCommand):
@@ -16,12 +17,31 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 setup(
-    name='Hypergraph',
-    version='0.1dev',
-    packages=['hypergraph'],
-    license='TBD',
-    long_description=open('README.md').read(),
-
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest}
+    name="halp",
+    packages=["halp", "Net"],
+    version="1.0.0",
+    description = "Hypergraph Algorithms Library in Python",
+    author = "Brendan Avent",
+    author_email = "bavent@vt.edu",
+    url = "http://tmmurali.github.io/halp/",
+    download_url = "https://github.com/tmmurali/halp/tarball/master",
+    keywords = ["hypergraph", "algorithms", "applications", "signaling", "graph", "network", "Hypergraph Theory"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics"
+        ],
+    license = "GNU GPLv2",
+    long_description=open("README.md").read(),
+    
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest}
 )
